adff8fc822f616ae49e13154069862ed082b8d2a
Thorfinn <komroncurabekov645@gmail.com>
2026-02-07 23:56:56 +0700

    Optimized code-->more readable #4[optimize]Closes #5 Fixes #5

--- httpffuzz.sh
+++ httpffuzz.sh
@@ -1,74 +1,80 @@
 #!/bin/bash
 
-cleanup() {
+cleanup(){
+    echo -e "\033[1;33m Usage[!] CTRL-C detected, stopping scan ...\033[0m"
+    kill 0
+    exit 1
+}
 
-  echo -e "\033[1;33mUsage[!] CTRL-C detected, stopping scan ... \033[0m"
-  kill 0
-  exit 1
+worker(){
 
+    local word="$1"
+
+    response=$(curl -k -s \
+    -o /dev/null \
+    --connect-timeout "$TIMEOUT" \
+    -A "$UA" \
+    -w "%{http_code}|%{size_download}|%{content_type}"\
+    "$url/$word"
+    )
+
+    code=$(echo "$response" | cut -d'|' -f1)
+    size=$(echo "$response" | cut -d'|' -f2)
+
+    if [[ "$code" -ne 404 && "$code" -ne 0 && "$size" != "$main_size" ]]; then
+        echo -e "\033[1;32m[$code]\033[0m $url/$word"
+    fi
 }
 
 trap cleanup INT
 
-url=""
+ulr=""
 wordls=""
 
-if [[ $# -ne 4 ]]; then
+if [[ $# -lt 4 ]]; then
     echo -e "\033[1;31m[!] Incorrect number of arguments\033[0m"
-    echo -e "\033[1;33mUsage: $0 -w [wordlist] -u [URL]\033[0m"
+    echo -e "\033[1;33m Usage: $0 -w [wordlist] - u [URL]\033[0m"
     exit 1
 fi
 
 if [[ "$1" == "-u" ]] && [[ "$3" == "-w"  ]]; then
-	url="$2"
-	wordls="$4"
+        url="$2"
+        wordls="$4"
 elif [[ "$1" == "-w" ]] && [[ "$3" == "-u" ]]; 
 then
-	url="$4"
-	wordls="$2"
+        url="$4"
+        wordls="$2"
 
 else
-	echo -e "\033[1;33m Incorrect usage of script:\n httpffuz -w [wordlist] -u [Url]\033[0m"
- 	exit 0
+        echo -e "\033[1;33m Incorrect usage of script:\n httpffuz -w [wordlist] -u [Url]\033[0m"
+        exit 0
 fi
 
 
-echo -e "Url: $url\nWordlist: $wordls"
+THREADS=20
+TIMEOUT=2
 
-THREADS=10
-TIMEOUT=1
-UA="Mozilla/5.0 (x11; Linux x86_64)"
-
+curr=0
+lns=$(wc -l < "$wordls")
 main_size=$(curl -k -s \
     -o /dev/null \
     --connect-timeout "$TIMEOUT" \
     -A "$UA" \
-    -w "%{size_download}" \
-    "$url"
-)
-
-
-while IFS= read -r word; do
-(
-response=$(curl -k -s \
-    -o /dev/null \
-    --connect-timeout "$TIMEOUT" \
-    -A "$UA" \
-    -w "%{http_code} %{size_download}"\
+    -w "%{size_download}"\
     "$url/$word"
 )
 
-code=$(echo "$response" | awk '{print $1}')
-size=$(echo "$response" | awk '{print $2}')
+while IFS= read -r word; do
+    ((curr++))
 
-	if [[ "$code" != "404" && "$code" != "000" && "$size" != "$main_size" ]]; then
-    		echo -e "\033[1;32m[$code]\033[0m $url/$word"
-	fi
-)&
+    worker "$word" &
+    
+    echo -ne "$curr/$lns\r"
 
-	while [ "$(jobs -r | wc -l)" -ge "$THREADS" ]; do
-		sleep 0.5
-	done
-done < $wordls
+    while [ $(jobs -r | wc -l) -ge "$THREADS" ]; do
+        sleep 0.1
+    done
 
+done < "$wordls"
 wait
+echo >&2
